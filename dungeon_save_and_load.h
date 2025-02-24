f559491c497060154e4a9d9418506c79da9ee2b3
collalti <jrcollalti@gmail.com>
2025-02-23 20:09:26 -0600

    Added files from 1.01 dungeon generation

--- dungeon_save_and_load.h
+++
@@ -1,9 +0,0 @@
-#ifndef DUNGEONSAVEANDLOAD_H
-#define DUNGEONSAVEANDLOAD_H
-
-#include "dungeon_generation.h"
-
-int save_dungeon(const char *path, Dungeon *dungeon);
-int load_dungeon(const char *path, Dungeon *dungeon);
-
-#endif
